826157f4e8eca4df2c2a3d4adfe644a3250eb4ca
Enes Yiğit <enestokyay2003@gmail.com>
2026-05-12 15:46:55 +0300

    Add initial index.php for project homepage

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <title>Proje Ana Sayfası</title>
+</head>
+<body>
+    <h1>Ortak PHP Projemize Hoş Geldiniz</h1>
+    <ul>
+        <li><a href="sayfa1.php">1. Arkadaşın Sayfası</a></li>
+        <li><a href="sayfa2.php">2. Arkadaşın Sayfası</a></li>
+        <li><a href="sayfa3.php">3. Arkadaşın Sayfası</a></li>
+    </ul>
+</body>
+</html>
